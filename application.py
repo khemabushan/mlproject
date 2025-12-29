41bd64da79a6f3d2336883249faba1f7832d44b8
demo <khemabushan@gmail.com>
2025-12-29 21:30:05 +0530

    Fix EB startup

--- application.py
+++ application.py
@@ -1,17 +1,14 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 app = application
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
@@ -24,8 +21,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
         )
 
         pred_df = data.get_data_as_data_frame()
@@ -34,6 +31,5 @@ def predict_datapoint():
 
         return render_template('home.html', results=results[0])
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    application.run()
